6d256465081c1f2dca48440bcd7f0d9bad57a1ac
Paul Min <info@paulmin.nl>
2022-01-18 08:46:41 +0100

    Rename

--- cross.sh
+++ cross.sh
@@ -7,4 +7,4 @@ set -o xtrace
 
 readonly LINK_FLAGS='-L /usr/aarch64-linux-gnu -L /usr/lib/aarch64-linux-gnu'
 
-RUSTFLAGS=${LINK_FLAGS} PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig cross build --release -p display-gtk --target aarch64-unknown-linux-gnu
+RUSTFLAGS=${LINK_FLAGS} PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig cross build --release --target aarch64-unknown-linux-gnu
