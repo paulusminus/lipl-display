e0744bbd864182697dd5d3812406127c2a3768e6
Paul Min <info@paulmin.nl>
2022-01-20 09:33:01 +0100

    Using stream branch of lipl-gatt-bluer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use anyhow::{Result};
-use futures::{StreamExt};
-use gtk::gio::prelude::*;
+use gtk::prelude::*;
 use gtk::glib::MainContext;
-use lipl_gatt_bluer::{listen, message::{Command, Message}};
-use gtk::prelude::WidgetExt;
+use lipl_gatt_bluer::{message::{Command, Message}};
 use tracing::{trace};
 
 mod css;
@@ -13,68 +11,56 @@ mod window;
 fn build_ui(application: &gtk::Application) -> Result<()> 
 {
     css::load(css::Theme::Dark);
-    let (cancel_tx, cancel_rx) = lipl_gatt_bluer::create_cancel();
-    let cancel_tx_clone = std::sync::Mutex::new(Some(cancel_tx));
-    let (values_tx, mut values_rx) = lipl_gatt_bluer::create_channel();
-    let _background = std::thread::spawn(move || {
-        trace!("Background task started");
-        if let Ok(runtime) = tokio::runtime::Builder::new_current_thread().enable_all().build() {
-            let _result = runtime.block_on(listen(cancel_rx, values_tx));
-        };
-
-    });
+    let (values_tx, values_rx) = MainContext::channel::<Message>(gtk::glib::source::PRIORITY_DEFAULT);
 
     let mut app_window = window::AppWindow::new(application)?;
     let window_clone = app_window.clone();
 
-    app_window.window.connect_delete_event(move |_,_| {
-        if let Some(mut tx) = cancel_tx_clone.lock().unwrap().take() {
-            let _result = tx.try_send(());
-        }
-        gtk::Inhibit(false)
+    lipl_gatt_bluer::listen_background(move |message| {
+        values_tx.send(message).map_err(|_| lipl_gatt_bluer::Error::Callback)
     });
 
-    MainContext::default().spawn_local(async move {
-        while let Some(value) = values_rx.next().await {
-            match value {
-                Message::Part(s) => {
-                    app_window.set_text(&s);
-                    trace!("Text updated");
-                },
-                Message::Status(s) => {
-                    app_window.set_status(&s);
-                    trace!("Status updated");
-                },
-                Message::Command(command) => {
-                    match command {
-                        Command::Increase => {
-                            app_window.increase_font_size();
-                            trace!("Increase font size");
-                        },
-                        Command::Decrease => {
-                            app_window.decrease_font_size();
-                            trace!("Decrease font size");
-                        },
-                        Command::Light => {
-                            css::load(css::Theme::Light);
-                            trace!("Light theme");
-                        },
-                        Command::Dark => {
-                            css::load(css::Theme::Dark);
-                            trace!("Dark theme");
-                        },
-                        Command::Exit => {
-                            window_clone.close();
-                            trace!("Exit");
-                        },
-                        Command::Poweroff => {
-                            window_clone.close();
-                            trace!("Poweroff");
-                        }
+    values_rx.attach(None, move |value| {
+        match value {
+            Message::Part(s) => {
+                app_window.set_text(&s);
+                trace!("Text updated");
+            },
+            Message::Status(s) => {
+                app_window.set_status(&s);
+                trace!("Status updated");
+            },
+            Message::Command(command) => {
+                match command {
+                    Command::Increase => {
+                        app_window.increase_font_size();
+                        trace!("Increase font size");
+                    },
+                    Command::Decrease => {
+                        app_window.decrease_font_size();
+                        trace!("Decrease font size");
+                    },
+                    Command::Light => {
+                        css::load(css::Theme::Light);
+                        trace!("Light theme");
+                    },
+                    Command::Dark => {
+                        css::load(css::Theme::Dark);
+                        trace!("Dark theme");
+                    },
+                    Command::Exit => {
+                        window_clone.close();
+                        trace!("Exit");
+                    },
+                    Command::Poweroff => {
+                        window_clone.close();
+                        trace!("Poweroff");
                     }
                 }
             }
         }
+
+        gtk::glib::Continue(true)
     });
 
     Ok(())
