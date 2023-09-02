a14144b0c342da850662bbd382cf1b879dda2dd3
paulusminus <info@paulmin.nl>
2023-09-02 12:34:46 +0200

    Publish lipl-display-femtovg

--- publish_crates_io.sh
+++ publish_crates_io.sh
@@ -4,5 +4,6 @@ cargo publish -p lipl-display-common
 cargo publish -p lipl-gatt-bluer
 cargo publish -p lipl-display-egui
 cargo publish -p lipl-display-gtk
+cargo publish -p lipl-display-femtovg
 cargo publish -p login-poweroff-reboot
 
