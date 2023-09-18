802cd6aa8e0b93fdd549f797dbe96268b70619c6
paulusminus <info@paulmin.nl>
2023-09-18 22:42:23 +0200

    Fix exiting

--- crates/lipl-display-slint/src/handle_message.rs
+++ crates/lipl-display-slint/src/handle_message.rs
@@ -1,20 +1,20 @@
 use lipl_display_common::{Message, Command};
 use log::error;
 use crate::LiplDisplay;
-use slint::{Weak, ComponentHandle};
+use slint::{invoke_from_event_loop, quit_event_loop, Weak};
 
 pub(crate) fn create_handle_message(ui_handle: Weak<LiplDisplay>) -> impl Fn(Message) {
     move |message| {
         match message {
             Message::Part(part) => {
                 let handle_copy = ui_handle.clone();
-                if let Err(error) = slint::invoke_from_event_loop(move || handle_copy.unwrap().set_part(part.into())) {
+                if let Err(error) = invoke_from_event_loop(move || handle_copy.unwrap().set_part(part.into())) {
                     error!("Error handling received part {}", error);
                 };
             },
             Message::Status(status) => {
                 let handle_copy = ui_handle.clone();
-                if let Err(error) = slint::invoke_from_event_loop(move || handle_copy.unwrap().set_status(status.into())) {
+                if let Err(error) = invoke_from_event_loop(move || handle_copy.unwrap().set_status(status.into())) {
                     error!("Error handling received status {}", error);
                 };
             },
@@ -22,19 +22,19 @@ pub(crate) fn create_handle_message(ui_handle: Weak<LiplDisplay>) -> impl Fn(Mes
                 match command {
                     Command::Dark => {
                         let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || handle_copy.unwrap().set_dark(true)) {
+                        if let Err(error) = invoke_from_event_loop(move || handle_copy.unwrap().set_dark(true)) {
                             error!("Error handling set theme dark {}", error);
                         };
                     },
                     Command::Light => {
                         let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || handle_copy.unwrap().set_dark(false)) {
+                        if let Err(error) = invoke_from_event_loop(move || handle_copy.unwrap().set_dark(false)) {
                             error!("Error handling set theme light {}", error);
                         };
                     },
                     Command::Increase => {
                         let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || {
+                        if let Err(error) = invoke_from_event_loop(move || {
                             let ui = handle_copy.unwrap();
                             let length = ui.get_fontsize();
                             ui.set_fontsize(length + 2);
@@ -44,7 +44,7 @@ pub(crate) fn create_handle_message(ui_handle: Weak<LiplDisplay>) -> impl Fn(Mes
                     },
                     Command::Decrease => {
                         let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || {
+                        if let Err(error) = invoke_from_event_loop(move || {
                             let ui = handle_copy.unwrap();
                             let length = ui.get_fontsize();
                             if length > 4 { ui.set_fontsize(length - 2) };
@@ -53,19 +53,13 @@ pub(crate) fn create_handle_message(ui_handle: Weak<LiplDisplay>) -> impl Fn(Mes
                         }
                     },
                     Command::Exit => {
-                        let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || 
-                            handle_copy.unwrap().window().dispatch_event(slint::platform::WindowEvent::CloseRequested)
-                        ) {
+                        if let Err(error) = quit_event_loop() {
                             error!("Failed to handle exit command {}", error);
                         }
                     },
                     Command::Poweroff => {
-                        let handle_copy = ui_handle.clone();
-                        if let Err(error) = slint::invoke_from_event_loop(move || 
-                            handle_copy.unwrap().window().dispatch_event(slint::platform::WindowEvent::CloseRequested)
-                        ) {
-                            error!("Failed to handle poweroff command {}", error);
+                        if let Err(error) = quit_event_loop() {
+                            error!("Failed to handle exit command {}", error);
                         }
                     }
                 }
