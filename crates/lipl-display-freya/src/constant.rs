8c1610057f3160f4335a519f23d8fee63c580430
Paul Min <info@paulmin.nl>
2026-09-05 10:34:47 +0200

    chore: update freya version

--- crates/lipl-display-freya/src/constant.rs
+++ crates/lipl-display-freya/src/constant.rs
@@ -2,11 +2,7 @@ pub const APPLICATION_TITLE: &str = "Lipl Display";
 pub const APPLICATION_WIDTH: f64 = 600.0;
 pub const APPLICATION_HEIGHT: f64 = 300.0;
 pub const WAIT_MESSAGE: &str = "Even geduld a.u.b. ...";
-pub const MINIMUM_FONT_SIZE: i32 = 4;
-pub const FONT_SIZE_INCREMENT: i32 = 2;
-
-#[allow(dead_code)]
-pub const PATH: &str = "/home/paul/Code/dart/lipl_display/lipl-gatt-input.txt";
+pub const FONT_SIZE: f32 = 46.0;
 
 #[cfg(feature = "fake")]
-pub const INPUT: &[u8] = include_bytes!("/home/paul/Code/dart/lipl_display/lipl-gatt-input.txt");
+pub const PATH: &str = "/home/paul/Code/dart/lipl_display/lipl-gatt-input.txt";
--- crates/lipl-display-freya/src/main.rs
+++ crates/lipl-display-freya/src/main.rs
@@ -3,140 +3,109 @@
     windows_subsystem = "windows"
 )]
 
-mod font_size;
-mod part;
-mod status;
-mod theme;
-
 #[cfg(feature = "fake")]
-use constant::INPUT;
-#[cfg(not(feature = "fake"))]
-use constant::PATH;
-use constant::{
-    APPLICATION_HEIGHT, APPLICATION_TITLE, APPLICATION_WIDTH, FONT_SIZE_INCREMENT,
-    MINIMUM_FONT_SIZE, WAIT_MESSAGE,
-};
-use font_size::FontSize;
-use freya::prelude::*;
-use futures_util::{FutureExt, StreamExt, TryStreamExt};
-use lipl_display_common::{Command, HandleMessage, LiplScreen, Message};
-use lipl_gatt_bluer::listen_stream;
-use part::Part;
-use status::Status;
 use std::time::Duration;
-use theme::Theme;
-use tokio::{runtime::Runtime, time::sleep};
+
+use constant::{APPLICATION_HEIGHT, APPLICATION_TITLE, APPLICATION_WIDTH};
+use freya::{prelude::*, sdk::use_track_watcher};
+#[cfg(feature = "fake")]
+use futures_util::TryStream;
+use futures_util::TryStreamExt;
+use lipl_display_common::{HandleMessage, LiplScreen, Message};
+use tokio::runtime::Runtime;
 
 mod constant;
-// mod file_input;
 
-async fn background_task() -> Result<(), std::io::Error> {
-    #[cfg(feature = "fake")]
-    let f = INPUT;
-    #[cfg(not(feature = "fake"))]
-    let f = json_lines::file_reader(PATH).await?;
-    json_lines::lines(f)
-        .try_for_each(move |message| {
-            match message {
-                Message::Part(p) => {
-                    consume_context::<Part>().set_text(p.into());
-                }
-                Message::Status(s) => consume_context::<Status>().set_text(s.into()),
-                Message::Command(c) => match c {
-                    Command::Dark => {
-                        consume_context::<Theme>().set(Theme::dark());
-                    }
-                    Command::Light => {
-                        consume_context::<Theme>().set(Theme::light());
-                    }
-                    Command::Increase => {
-                        let mut font_size = consume_context::<FontSize>();
-                        let f = font_size.value();
-                        font_size.set(f + FONT_SIZE_INCREMENT);
-                    }
-                    Command::Decrease => {
-                        let font_size = consume_context::<FontSize>().value();
-                        if font_size > MINIMUM_FONT_SIZE {
-                            consume_context::<FontSize>().set(font_size - FONT_SIZE_INCREMENT);
-                        }
-                    }
-                    Command::Wait => {
-                        consume_context::<Part>().set_text("".to_owned().into());
-                        consume_context::<Status>().set_text(WAIT_MESSAGE.to_owned().into());
-                    }
-                    Command::Exit => {
-                        // use_platform().close_window();
-                    }
-                    Command::Poweroff => {
-                        // use_platform().close_window();
-                    }
-                },
-            }
-            sleep(Duration::from_secs(1)).map(|_| Ok(()))
-        })
-        .await
+struct DisplayApp {
+    rx: tokio::sync::watch::Receiver<LiplScreen>,
 }
 
-#[derive(PartialEq)]
-struct Root;
-
-impl Component for Root {
+impl App for DisplayApp {
     fn render(&self) -> impl IntoElement {
-        let theme = use_state(|| Theme::Dark);
-        let font_size = use_state(|| FontSize::from(22));
-        let status = use_state(|| Status::default());
-        let part = use_state(|| Part::default());
-
-        use_future(background_task);
+        use_track_watcher(&self.rx);
 
         rect().children([
             rect()
                 .width(Size::percent(100.0))
                 .height(Size::percent(90.0))
-                .background(Fill::Color(theme.read().bg_color()))
-                .color(Fill::Color(theme.read().fg_color()))
-                .font_size(freya::prelude::FontSize::from(font_size.read().value()))
+                .background(Fill::Color(if self.rx.borrow().dark {
+                    Color::BLACK
+                } else {
+                    Color::WHITE
+                }))
+                .color(Fill::Color(if self.rx.borrow().dark {
+                    Color::WHITE
+                } else {
+                    Color::BLACK
+                }))
+                .font_size(FontSize::from(self.rx.borrow().font_size))
                 .padding(Gaps::new_all(20.0))
-                .children([label().text(part.read().to_string()).into_element()])
+                .children([label().text(self.rx.borrow().text.clone()).into_element()])
                 .into_element(),
             rect()
                 .width(Size::percent(100.0))
                 .height(Size::percent(10.0))
-                .background(Fill::Color(theme.read().fg_color()))
-                .color(Fill::Color(theme.read().bg_color()))
+                .background(Fill::Color(if self.rx.borrow().dark {
+                    Color::BLACK
+                } else {
+                    Color::WHITE
+                }))
+                .font_size(FontSize::from(self.rx.borrow().font_size))
                 .padding(Gaps::new_all(20.0))
-                .children([label().text(status.read().to_string()).into_element()])
+                .children([label().text(self.rx.borrow().status.clone()).into_element()])
                 .into_element(),
         ])
     }
 }
 
-#[derive(Default)]
-struct DisplayApp;
+#[cfg(feature = "fake")]
+async fn listen() -> impl TryStream<Ok = Message, Error = std::io::Error> {
+    let lines = json_lines::file_reader(constant::PATH).await.unwrap();
+    json_lines::lines(lines)
+}
 
-impl App for DisplayApp {
-    fn render(&self) -> impl IntoElement {
-        Root {}
-    }
+#[cfg(not(feature = "fake"))]
+async fn listen() -> impl TryStream<Ok = Message, Error = std::io::Error> {
+    let s = lipl_gatt_bluer::listen_stream().await.unwrap();
+    s
 }
 
 fn main() {
     let rt = Runtime::new().unwrap();
     let _guard = rt.enter();
 
-    let window_config = WindowConfig::new_app(DisplayApp::default())
+    let (tx, rx) = tokio::sync::watch::channel(LiplScreen::default());
+
+    let window_config = WindowConfig::new_app(DisplayApp { rx })
         .with_title(APPLICATION_TITLE)
-        .with_size(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+        .with_size(APPLICATION_WIDTH, APPLICATION_HEIGHT)
+        .with_window_attributes(|attributes, _| {
+            attributes.with_fullscreen(Some(freya::winit::window::Fullscreen::Borderless(None)))
+        })
+        .with_window_handle(|window| {
+            window.set_cursor_visible(false);
+        });
     let launch_config = LaunchConfig::default()
         .with_window(window_config)
-        .with_future(|proxy| async {
-            let s = listen_stream()
-                .await
-                .unwrap()
-                .scan(LiplScreen::default(), |last, message| {
-                    last.handle_message(message);
-                    async move { Some(last.clone()) }
-                });
+        .with_future(|_proxy: LaunchProxy| async move {
+            let mut s = listen().await;
+            let mut screen = LiplScreen::default();
+            screen.font_size = constant::FONT_SIZE;
+            screen.status = constant::WAIT_MESSAGE.to_string();
+
+            while let Ok(Some(message)) = s.try_next().await {
+                if message.is_stop() {
+                    let result = _proxy.post_callback(|render_context| {
+                        render_context.exit();
+                    });
+                    result.await.unwrap();
+                    break;
+                }
+                screen.handle_message(message);
+                tx.send(screen.clone()).unwrap();
+                #[cfg(feature = "fake")]
+                tokio::time::sleep(Duration::from_millis(1000)).await;
+            }
         });
     launch(launch_config);
 }
